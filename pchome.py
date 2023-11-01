6c6ffb59630384fc0422f1acea7698777bed45f1
huangcode0620 <122358708+huangcode0620@users.noreply.github.com>
2023-11-01 18:38:24 +0800

    PcHome資料抓取練習
    PcHome資料抓取練習

---
+++ pchome.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 20:34:54 2023
+
+@author: USER
+"""
+
+import requests
+import json
+header = {
+    'User-Agent':
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+
+url = "https://ecshweb.pchome.com.tw/search/v4.3/all/results"
+
+for i in range(1,5):
+
+    param = {
+        'q': '登機箱',
+    'page': i,
+    'sort': 'rnk/dc'
+        }
+    
+    
+    
+    data = requests.get(url,headers=header,params=param).text
+    
+    pchome = json.loads(data)
+    
+    goods =  pchome['Prods']
+    
+    
+    for item in goods:
+        title = item['Name']
+        photo = "https://cs-b.ecimg.tw" + item['PicB']
+        price = item['Price']
+        info = item['Describe']
+        
+        print(title)
+        print(photo)
+        print(price)
+        print(info)
+        
+        print()
+    
+    
+    
+
